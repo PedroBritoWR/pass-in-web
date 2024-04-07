a27a16873e9784ed98ed50b7203b699d47c0eda2
PedroBritoWR <pedrowpvh@hotmail.com>
2024-04-06 23:59:00 -0400

    first commit

--- src/components/Attendee-list.tsx
+++ src/components/Attendee-list.tsx
@@ -7,36 +7,99 @@ import { Table } from "./table/Table";
 import { TableHeader } from "./table/TableHeader";
 import { TableCell } from "./table/TableCell";
 import { TableRow } from "./table/TableRow";
-import { ChangeEvent, useState } from "react";
-import { attendees } from "./data/attendee";
+import { ChangeEvent, useEffect, useState } from "react";
 
 dayjs.extend(relativeTime)
 dayjs.locale('pt-br')
 
+interface attendee {
+  id: string
+  name: string
+  email: string
+  createdAt: string
+  checkedInAt: string | null
+}
+
  export function Participantes () {
 
-    const [input, setInput] = useState('')
-    const [page, setPage] = useState (1)
-    const totalPages = Math.ceil(attendees.length / 10)
+    const [search, setSearch] = useState(() => {
+    const url = new URL (window.location.toString());
 
-    function InputChange(event:ChangeEvent <HTMLInputElement> ) {
-        setInput(event.target.value)
+      if (url.searchParams.has('search')) {
+        return  (url.searchParams.get('search'))  ?? ""
+      }
+
+      return ""
+    })
+    const [page, setPage] = useState (() => {
+    const url = new URL (window.location.toString())
+
+      if(url.searchParams.has('page')) {
+        return Number (url.searchParams.get("page")); 
+      }
+
+      return 1
+
+    })
+    const [total, setTotal] = useState (0)
+    const [attendees, setAttendees] = useState<attendee[]>([])
+    const totalPages = Math.ceil(total / 10)
+
+    useEffect(() => {
+      const url = new URL(
+        'http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees?'
+      );
+
+      url.searchParams.set("pageIndex", String (page - 1));
+      if (search.length > 1) {
+        url.searchParams.set("query", search )
+      }
+
+
+      fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setAttendees(data.attendees)
+        setTotal(data.total)
+      })
+    }, [page, search] )
+
+    function setCurrentSearch(search: string){
+      const url = new URL(window.location.toString())
+      url.searchParams.set('search', search)
+      window.history.pushState({}, "", url)
+      setSearch(search)
     }
 
+    function setCurrentPage (page: number) {
+      const url = new URL(window.location.toString());
+      url.searchParams.set("page", String (page))
+      window.history.pushState({}, "", url)
+      setPage(page)
+
+    }
+
+    function InputChange(event:ChangeEvent <HTMLInputElement> ) {
+        setCurrentSearch(event.target.value)
+        setCurrentPage (1)
+    }
+
+
     function goToFirstPage() {
-      setPage(1)
+      setCurrentPage (1)
     }
 
     function goToPreviousPage () {
-      setPage (page -1)
+      setCurrentPage (page -1)
     }
 
     function goToNextPage () {
-      setPage(page + 1)
+      setCurrentPage( page + 1)
     }
 
     function goToLastPage () {
-      setPage(totalPages)
+      setCurrentPage (totalPages)
+
     }
 
     return (
@@ -45,9 +108,10 @@ dayjs.locale('pt-br')
                 <h1 className="font-Roboto text-2xl ">Participantes</h1>
               <div className=" w-72 px-3 py-1.5  border-white/10  border rounded-lg text-sm flex gap-3 items-center ">
                 <Search className="size-4 text-emerald-300 " />
-                <input onChange={InputChange} className="bg-transparent border-0 px-2 text-sm focus:ring-0" placeholder="Buscar participante..." />
+                <input onChange={InputChange} className="bg-transparent border-0 px-2 text-sm focus:ring-0" placeholder="Buscar participante..." 
+                value={search}
+                />
               </div>
-              {input}
             </div>
             <Table>
               <thead>
@@ -64,7 +128,7 @@ dayjs.locale('pt-br')
                 </TableRow>
                 </thead>
             <tbody>
-              {attendees.slice((page -1) *10, page*10 ).map((attendee) => {
+              {attendees.map((attendee) => {
                 return (
                   <TableRow key={attendee.id}>
                     <TableCell className="py-3 px-4 text-sm text-zinc-300">
@@ -80,7 +144,7 @@ dayjs.locale('pt-br')
                       </div>
                     </TableCell>
                     <TableCell>{dayjs().to(attendee.createdAt)} </TableCell>
-                    <TableCell>{dayjs().to(attendee.checkedInAt)} </TableCell>
+                    <TableCell>{attendee.checkedInAt === null ? <span className="text-zinc-400">não fez check-in</span> : dayjs().to(attendee.checkedInAt)} </TableCell>
                     <TableCell>
                       <IconButtonRow transparent={true}>
                         <MoreHorizontal className="size-5" />
@@ -92,10 +156,10 @@ dayjs.locale('pt-br')
             </tbody>
             <tfoot>
               <tr>
-                <TableCell colSpan={3}> Mostrando 10 de { attendees.length } itens </TableCell>
+                <TableCell colSpan={3}>Mostrando {attendees.length} de { total } itens </TableCell>
                 <TableCell className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>
                 <div>
-                  <span>Página {page} de {} </span>
+                  <span>Página {page} de {totalPages} </span>
                 <div className="inline-flex items-center gap-2 mx-4">
                     <IconButtonRow onClick={goToFirstPage} disabled={page === 1}
                         transparent={false}>              
--- src/components/IconButtonRow.tsx
+++ src/components/IconButtonRow.tsx
@@ -1,16 +1,23 @@
 import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface IconButtonRowProps extends ComponentProps <'button'> {
     transparent: boolean
 }
 
+// ? "bg-black/20 border border-white/10 rounded-md p-1.5 hover:bg-black/80" 
+// : "bg-white/10 border border-white/10 rounded-md p-1.5 hover:bg-black/80 " }>
+
 export function IconButtonRow ({transparent, ...props}:IconButtonRowProps) {
     return(
-        <button {...props}
-         className={
-            transparent 
-            ? "bg-black/20 border border-white/10 rounded-md p-1.5 hover:bg-black/80" 
-            : "bg-white/10 border border-white/10 rounded-md p-1.5 hover:bg-black/80 " }>
-         </button>
-    )
+        <button 
+        {...props}
+            className={twMerge(
+                "bg-black/20 border border-white/10 rounded-md p-1.5",
+                transparent ? 'bg-black/20' : 'bg-white/10',
+                props.disabled ? 'opacity-50' : null
+
+            )}
+        />
+     )
 }
